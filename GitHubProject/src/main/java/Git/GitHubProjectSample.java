c1ba0d7048fea08490e04f34c7ff6ecc6386bbe2
Hariprasath <Hariprasath@Testing-Sys337>
2025-04-22 17:53:10 +0530

    Git commit

---
+++ GitHubProject/src/main/java/Git/GitHubProjectSample.java
@@ -0,0 +1,18 @@
+package Git;
+
+public class GitHubProjectSample {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Suresh Raina->03");
+		
+		System.out.println("Virat Kohli->18");
+		
+		System.out.println("MSD->07");
+		
+		System.out.println("Rohit Sharma->10");
+
+	}
+
+}
